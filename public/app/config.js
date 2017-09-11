1176eb8776118d7d5c6134b97cabe0ce0c8561dd
Hemesh Unka <hemeshunka@gmail.com>
2017-09-11 21:35:34 +0100

    track package.json

--- public/app/config.js
+++ public/app/config.js
@@ -4,8 +4,7 @@ require.config({
 		'underscore': 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min',
 		'backbone': 'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min',
 		'backbone.layoutmanager': 'https://cdnjs.cloudflare.com/ajax/libs/backbone.layoutmanager/1.0.0/backbone.layoutmanager.min',
-		'dygraph': 'https://cdnjs.cloudflare.com/ajax/libs/dygraph/2.0.0/dygraph.min.js',
-		'dygraphcss': 'https://cdnjs.cloudflare.com/ajax/libs/dygraph/2.0.0/dygraph.min.css',
+		'dygraph': 'https://cdnjs.cloudflare.com/ajax/libs/dygraph/2.0.0/dygraph.min',
 		
 	},
 	shim: {
