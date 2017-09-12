1dd103ffd5cb0e8a8841f833517c029792ccb1c3
Hemesh Unka <hemeshunka@gmail.com>
2017-09-12 14:53:11 +0100

    track package.json

--- public/app/js/modules/shelf.js
+++ public/app/js/modules/shelf.js
@@ -54,12 +54,10 @@ define("js/modules/shelf", ["jquery", "underscore", "backbone", "app", "dygraph"
 			
 			_.map(obj, function(value) {
 					
-				newObj[] = _.values(value);
+				newObj = _.values(value);
 			
 			});
-			
 				return newObj;
-				
 		  }
 		  
 		}),
