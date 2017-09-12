c64556254c26b9f3c099c68854b935e69e46576b
Hemesh Unka <hemeshunka@gmail.com>
2017-09-12 16:38:47 +0100

    track package.json

--- public/app/js/modules/shelf.js
+++ public/app/js/modules/shelf.js
@@ -42,7 +42,7 @@ define("js/modules/shelf", ["jquery", "underscore", "backbone", "app", "dygraph"
 				//2009/07/12 12:34:56
 
 				
-				g = new x(document.getElementById("graph"), [new Date("2009/07/12"), 6.49],[new Date("2009/07/16"), 6.99], {
+				g = new x(document.getElementById("graph"), [["2009/07/12", 6.49],"2009/07/16", 6.99]], {
 					labels: [ "Date", "Price" ],
 				}
 				);
