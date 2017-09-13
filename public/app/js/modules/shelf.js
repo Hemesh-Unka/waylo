9782176968b1f6fb7bb529dd154310e6fe796259
Hemesh Unka <hemeshunka@gmail.com>
2017-09-13 11:45:51 +0100

    track package.json

--- public/app/js/modules/shelf.js
+++ public/app/js/modules/shelf.js
@@ -37,29 +37,28 @@ define("js/modules/shelf", ["jquery", "underscore", "backbone", "app", "c3", "d3
 												
 				console.log(obj);
 				
-var chart = c3.generate({
-    bindTo: '#chart',
-    data: {
-        x: 'x',
-        xFormat: '%Y-%m-%dT%H:%M:%S.%LZ',
-/*
-        columns: [
-            ['x', '2015-09-09T01:05:43.269Z', '2015-09-09T02:05:43.269Z', '2015-09-09T03:05:43.269Z', '2015-09-09T04:05:43.269Z', '2015-09-09T05:05:43.269Z'],
-            ['sample', 30, 100, 400, 150], ]
-*/
-         json: obj,
-    },
-    axis: {
-            value: ['Date', 'Price'],
-        x: {
-            type: 'timeseries',
-            tick: {
-                fit: false,
-                format: "%H:%M"
-            }
-        }
-    }
-});
+		var chart = c3.generate({
+			bindTo: '#chart',
+				data: {
+					//x: 'x',
+					//xFormat: '%Y-%m-%dT%H:%M:%S.%LZ',
+					/*
+					columns: [
+					['x', '2015-09-09T01:05:43.269Z', '2015-09-09T02:05:43.269Z', '2015-09-09T03:05:43.269Z', '2015-09-09T04:05:43.269Z', '2015-09-09T05:05:43.269Z'],
+					['sample', 30, 100, 400, 150], ]
+					*/
+					json: obj,
+				},
+				keys: {
+					// x: 'name', // it's possible to specify 'x' when category axis
+					value: ['upload', 'download']
+				}
+			},
+			axis: {
+				x: {
+				// type: 'category'
+			}
+		});
         
 		  },
 		  
