62810c3a20bedcf9d84313c2e44bca22ecc48abd
Hemesh Unka <hemeshunka@gmail.com>
2017-09-13 11:05:15 +0100

    track package.json

--- public/app/config.js
+++ public/app/config.js
@@ -3,7 +3,8 @@ require.config({
 		'jquery': 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min',
 		'underscore': 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min',
 		'backbone': 'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min',
-		'backbone.layoutmanager': 'https://cdnjs.cloudflare.com/ajax/libs/backbone.layoutmanager/1.0.0/backbone.layoutmanager.min',		
+		'backbone.layoutmanager': 'https://cdnjs.cloudflare.com/ajax/libs/backbone.layoutmanager/1.0.0/backbone.layoutmanager.min',
+		'c3': 'https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.17/c3.min',		
 	},
 	shim: {
 		'backbone': {
