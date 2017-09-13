492b0fc19191c66901d1f4562dd3623777a9b378
Hemesh Unka <hemeshunka@gmail.com>
2017-09-13 11:59:18 +0100

    track package.json

--- public/app/js/modules/shelf.js
+++ public/app/js/modules/shelf.js
@@ -37,49 +37,24 @@ define("js/modules/shelf", ["jquery", "underscore", "backbone", "app", "c3", "d3
 												
 				console.log(obj);
 				
-var chart = c3.generate({
-  data: {
-    json: obj,
-    keys: {
-      // x: 'name', // it's possible to specify 'x' when category axis
-      value: ['price', 'date']
-    }
-  },
-    axis: {
-        x: {
-            type: 'timeseries',
-            tick: {
-                format: '%Y-%m-%dT%H:%M:%S.%LZ'
-            }
-        }
-    }
-});
-
-/*
-		var chart = c3.generate({
-			bindTo: '#chart',
-				data: {
-					//x: 'x',
-					//xFormat: '%Y-%m-%dT%H:%M:%S.%LZ',
-					/*
-					columns: [
-					['x', '2015-09-09T01:05:43.269Z', '2015-09-09T02:05:43.269Z', '2015-09-09T03:05:43.269Z', '2015-09-09T04:05:43.269Z', '2015-09-09T05:05:43.269Z'],
-					['sample', 30, 100, 400, 150], ]
-					*/
-				//	json: obj,
-				//},
-				//keys: {
-					// x: 'name', // it's possible to specify 'x' when category axis
-				//	value: ['date', 'price']
-				//}
-			//},
-			  //axis: {
-			    //x: {
-			      // type: 'category'
-			    //}
-			  //}
-		//});
-        
+				var chart = c3.generate({
+				  data: {
+				    json: obj,
+				    keys: {
+				      x: 'date', // it's possible to specify 'x' when category axis
+				      value: ['price', 'date']
+				    }
+				  },
+				    axis: {
+				        x: {
+				            type: 'timeseries',
+				            tick: {
+				                format: '%Y-%m-%dT%H:%M:%S.%LZ'
+				            }
+				        }
+				    }
+				});
+       
 		  },
 		  
 		}),
