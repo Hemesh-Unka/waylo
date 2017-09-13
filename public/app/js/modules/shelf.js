c3812d6330974b2e1bfc6e8faced4f4ba247044e
Hemesh Unka <hemeshunka@gmail.com>
2017-09-13 12:34:51 +0100

    track package.json

--- public/app/js/modules/shelf.js
+++ public/app/js/modules/shelf.js
@@ -37,10 +37,13 @@ define("js/modules/shelf", ["jquery", "underscore", "backbone", "app", "c3", "d3
 																
 			var chart = c3.generate({
 			  data: {
+			    
 			    json: obj,
 				xFormat: '%Y-%m-%d %H:%M:%S',
+			
+			},
 				
-			    keys: {
+			keys: {
 			      x: 'date', // it's possible to specify 'x' when category axis
 			      value: ['date', 'price']
 			    }
