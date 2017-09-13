3ca8ea3d0a76709507a757141e19e290325f14b1
Hemesh Unka <hemeshunka@gmail.com>
2017-09-13 11:07:07 +0100

    track package.json

--- public/app/js/modules/shelf.js
+++ public/app/js/modules/shelf.js
@@ -39,15 +39,15 @@ define("js/modules/shelf", ["jquery", "underscore", "backbone", "app", "c3"], fu
 				
 				console.log(obj);
 				
-/*
-			g = new Dygraph(document.getElementById('graph'),
-				[[1247382000000, 10],
-				 [1247468400000, 20],
-				 [1247554800000, 30]],
-				{
-				    labels: ['Date', 'Value'],
+			var chart = c3.generate({
+			    bindto: '#graph',
+			    data: {
+			      columns: [
+			        ['data1', 30, 200, 100, 400, 150, 250],
+			        ['data2', 50, 20, 10, 40, 15, 25]
+			      ]
+			    }
 			});
-*/
         
 		  },
 		  
