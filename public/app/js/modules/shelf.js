c97bcad796a32c13973e625758f36b42779ce8de
Hemesh <hemesh@Hemeshs-MacBook-Air.local>
2017-11-22 10:12:46 +0000

    Changed html framework.

--- public/app/js/modules/shelf.js
+++ public/app/js/modules/shelf.js
@@ -73,8 +73,6 @@ define("js/modules/shelf", ["jquery", "underscore", "backbone", "app", "c3", "d3
 
 			template: _.template( $( '#productTemplate' ).html() ),
 
-			tagName: 'div',
-
 			className: 'col-sm productPreview',
 
 			render: function(layout) {
