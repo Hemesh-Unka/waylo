b0b2e8a5cfc1036dfac126bd53525e60d0b711c2
Hemesh <hemesh@Hemeshs-MacBook-Air.local>
2017-11-23 15:20:01 +0000

    First commit

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.get('/api/uri', function (req, res) {
       docsLength = docs.length;
 
       for (var i = 0; i < docsLength; i++) {
-        console.log(docs[i]);
+        console.log(docs[i].title);
       }
 
       //res.status(200.json(test);
