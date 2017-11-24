4fc21127e54a81982dcaec3bf44aadd1cf6d36e1
Hemesh <hemesh@Hemeshs-MacBook-Air.local>
2017-11-24 19:15:00 +0000

    First commit

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/api/uri', function (req, res) {
       handleError(res, err.message, 'Failed to get products.');
     } else {
 
-      console.log(docs);
+      console.log(docs.title);
 
       //res.send(docs.title);
     }
