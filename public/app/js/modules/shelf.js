d1bac323e837bda3a526b5c1092058b52b78151a
Hemesh <hemesh@Hemeshs-MacBook-Air.local>
2017-11-27 14:49:40 +0000

    added min and max lines

--- public/app/js/modules/shelf.js
+++ public/app/js/modules/shelf.js
@@ -75,8 +75,8 @@ define('js/modules/shelf', ['jquery', 'underscore', 'backbone', 'app', 'c3', 'd3
             },
             y: {
               lines: [
-                  { value: this.minOrMax(obj, "max"), text: 'Max Price', position: 'finish' },
-                  { value: this.minOrMax(obj, "min"), text: 'Min Price', position: 'start' },
+                  { value: this.max(obj), text: 'Max Price', position: 'finish' },
+                  { value: this.min(obj), text: 'Min Price', position: 'start' },
               ],
               tick: {
                 format: d3.format(',.2f'),
